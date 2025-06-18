8ff153e53e6391e02727dab77b8e88a7b021283f
adamagicic <aagicic.phy@pmf.hr>
2025-06-18 17:26:53 +0200

    New ANN and dataset.

--- datasets/run353dataset/run353dataset_dataset_builder.py
+++ datasets/run353dataset/run353dataset_dataset_builder.py
@@ -8,9 +8,9 @@ import csv
 class Builder(tfds.core.GeneratorBasedBuilder):
   """DatasetBuilder for run353dataset dataset."""
 
-  VERSION = tfds.core.Version('1.1.2')
+  VERSION = tfds.core.Version('1.3.1')
   RELEASE_NOTES = {
-      '1.1.2': 'Finalized point selection; no more noisy data.',
+      '1.3.1': 'New idea for training label - dt = t_mm-t_0mm ----> SAT = t_mcp - t_0 - t_ANN.',
   }
   MANUAL_DOWNLOAD_INSTRUCTIONS = """the raw data is not currently available for download"""
 
@@ -21,7 +21,10 @@ class Builder(tfds.core.GeneratorBasedBuilder):
         features=tfds.features.FeaturesDict({
             # These are the features of your dataset like images, labels ...
             'eventNo': tfds.features.Scalar(dtype = np.int64, doc = 'event number (ID)'),
-            'time_label': tfds.features.Scalar(dtype = np.float64, doc = 'PICOSEC-MM arrival time from full signal analysis'),
+            'sat': tfds.features.Scalar(dtype = np.float64, doc = 'PICOSEC-MM arrival time from full signal analysis'),
+            't_mcp': tfds.features.Scalar(dtype = np.float64, doc = 'timepoint 20% of MCP signal amplitude (sigmoid fit)'),
+            't_mm': tfds.features.Scalar(dtype = np.float64, doc = 'timepoint 20% of MM signal amplitude (sigmoid fit)'),
+            't_0mm': tfds.features.Scalar(dtype = np.float64, doc = 'timepoint of beginning of signal sample'),
             'signal_data': tfds.features.Tensor(shape = (64,), dtype = np.float64, doc = '64 signal points for ANN input'),
         }),
         # If there's a common (input, target) tuple from the
@@ -34,12 +37,12 @@ class Builder(tfds.core.GeneratorBasedBuilder):
   def _split_generators(self, dl_manager: tfds.download.DownloadManager):
     """Returns SplitGenerators."""
     # TODO(run353dataset): Downloads the data and defines the splits
-    path = dl_manager.manual_dir / 'Run353.zip'
+    path = dl_manager.manual_dir / 'Run353ANNdelta.zip'
     extracted_path = dl_manager.extract(path)
 
     # TODO(run353dataset): Returns the Dict[split names, Iterator[Key, Example]]
     return {
-        'train': self._generate_examples(extracted_path / 'Run353.csv'),
+        'train': self._generate_examples(extracted_path / 'Run353ANNdelta.csv'),
     }
 
   def _generate_examples(self, path):
@@ -50,6 +53,9 @@ class Builder(tfds.core.GeneratorBasedBuilder):
         key = row['eventNo']
         yield key, {
           'eventNo': row['eventNo'],
-          'time_label': row['time_label'],
+          'sat': row['sat'],
+          't_mcp': row['t_mcp'],
+          't_mm': row['t_mm'],
+          't_0mm': row['t_0mm'],
           'signal_data': np.fromstring(row['signal_data'].replace("[", "").replace("]", "").replace("\n", ""), dtype = np.float64, sep = ' '),
       }
